7f032a0af0603e3cb6c37178452e3f3b5026ffad
Pavelchak <apavelchak@gmail.com>
2017-12-01 00:07:10 +0200

    added integratedSecurity=true

--- src/main/java/com/softserve/jdbctest/Aplication.java
+++ src/main/java/com/softserve/jdbctest/Aplication.java
@@ -5,28 +5,31 @@ import java.util.Scanner;
 
 
 public class Aplication {
-//    private static final String url = "jdbc:sqlserver://ANDRIY-PC:1433;database=DB_JDBC;user=sa;password=1";
-private static final String url ="jdbc:sqlserver://ANDRIY-PC:1433;database=DB_JDBC";
+//        private static final String url = "jdbc:sqlserver://ANDRIY-PC:1433;database=DB_JDBC;user=sa;password=1";
+    private static final String url = "jdbc:sqlserver://ANDRIY-PC:1433;database=DB_JDBC";
+//    private static final String url = "jdbc:sqlserver://localhost:1433;database=DB_JDBC;integratedSecurity=true;";
+
     private static final String user = "sa";
     private static final String password = "1";
 
-    private static Connection connection=null;
-    private static Statement statement=null;
-    private static ResultSet rs=null;
+    private static Connection connection = null;
+    private static Statement statement = null;
+    private static ResultSet rs = null;
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         try {
 //region    0. This will load the MySQL driver, each DB has its own driver //
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             //endregion
 
 //region    1. Get a connection to database //
-            connection = DriverManager.getConnection(url, user, password);
+            connection = DriverManager.getConnection(url,user,password);
+//            connection = DriverManager.getConnection(url);
             //endregion
 
 //region  2. Create a statement
             // Statements allow to issue SQL queries to the database
-            statement=connection.createStatement();
+            statement = connection.createStatement();
             //endregion
 
             //readData();
@@ -51,96 +54,101 @@ private static final String url ="jdbc:sqlserver://ANDRIY-PC:1433;database=DB_JD
 
         } finally {
             //close connection ,statement and resultset
-            if (rs != null) try { rs.close(); } catch (SQLException e) { } // ignore
-            if (statement != null) try { statement.close(); } catch (SQLException e) { }
-            if (connection != null) try { connection.close(); } catch (SQLException e) { }
+            if (rs != null) try {
+                rs.close();
+            } catch (SQLException e) {
+            } // ignore
+            if (statement != null) try {
+                statement.close();
+            } catch (SQLException e) {
+            }
+            if (connection != null) try {
+                connection.close();
+            } catch (SQLException e) {
+            }
         }
     }
 
     private static void readData() throws SQLException {
 //region    SELECT COUNT(*) FROM Person //
-            // 3. executing SELECT query
-            rs=statement.executeQuery("SELECT COUNT(*) FROM Person");
+        // 3. executing SELECT query
+        rs = statement.executeQuery("SELECT COUNT(*) FROM Person");
 
-            // 4. Process the result set
-            while (rs.next()) {
-                int count = rs.getInt(1);
-                // Simply Print the results
-                System.out.format("\ncount: %d\n", count);
-            }
-            //endregion
+        // 4. Process the result set
+        while (rs.next()) {
+            int count = rs.getInt(1);
+            // Simply Print the results
+            System.out.format("\ncount: %d\n", count);
+        }
+        //endregion
 
 //region    SELECT * FROM Person //
-            // 3. executing SELECT query
-            rs=statement.executeQuery("SELECT * FROM Person");
+        // 3. executing SELECT query
+        rs = statement.executeQuery("SELECT * FROM Person");
 
-            // 4. Process the result set
-            System.out.format("\nTable Person --------------------\n");
-            System.out.format("%3s %-12s %-12s %-10s %s\n", "ID", "Surname", "Name", "City", "Email");
-            while (rs.next())
-            {
-                int id=rs.getInt("IDPerson");
-                String surname = rs.getString("Surname");
-                String name = rs.getString("Name");
-                String city=rs.getString("City");
-                String email=rs.getString("Email");
-                // Simply Print the results
-                System.out.format("%3d %-12s %-12s %-10s %s\n", id, surname, name, city, email);
-            }
-            //endregion
+        // 4. Process the result set
+        System.out.format("\nTable Person --------------------\n");
+        System.out.format("%3s %-12s %-12s %-10s %s\n", "ID", "Surname", "Name", "City", "Email");
+        while (rs.next()) {
+            int id = rs.getInt("IDPerson");
+            String surname = rs.getString("Surname");
+            String name = rs.getString("Name");
+            String city = rs.getString("City");
+            String email = rs.getString("Email");
+            // Simply Print the results
+            System.out.format("%3d %-12s %-12s %-10s %s\n", id, surname, name, city, email);
+        }
+        //endregion
 
 //region    SELECT * FROM Book //
-            // 3. executing SELECT query
-            rs=statement.executeQuery("SELECT * FROM Book");
+        // 3. executing SELECT query
+        rs = statement.executeQuery("SELECT * FROM Book");
 
-            // 4. Process the result set
-            System.out.format("\nTable Book --------------------\n");
-            System.out.format("%3s %-18s %-18s %s\n", "ID", "BookName", "Author", "Amount");
-            while (rs.next())
-            {
-                int id=rs.getInt("IDBook");
-                String bookName = rs.getString("BookName");
-                String author = rs.getString("Author");
-                String amount=rs.getString("Amount");
-                // Simply Print the results
-                System.out.format("%3d %-18s %-18s %s\n", id, bookName, author, amount);
-            }
-            //endregion
+        // 4. Process the result set
+        System.out.format("\nTable Book --------------------\n");
+        System.out.format("%3s %-18s %-18s %s\n", "ID", "BookName", "Author", "Amount");
+        while (rs.next()) {
+            int id = rs.getInt("IDBook");
+            String bookName = rs.getString("BookName");
+            String author = rs.getString("Author");
+            String amount = rs.getString("Amount");
+            // Simply Print the results
+            System.out.format("%3d %-18s %-18s %s\n", id, bookName, author, amount);
+        }
+        //endregion
 
 //region    SELECT * FROM City //
-            // 3. executing SELECT query
-            rs=statement.executeQuery("SELECT * FROM City");
+        // 3. executing SELECT query
+        rs = statement.executeQuery("SELECT * FROM City");
 
-            // 4. Process the result set
-            System.out.format("\nTable City --------------------\n");
-            System.out.format("%s\n", "City");
-            while (rs.next())
-            {
-                String city = rs.getString("City");
-                // Simply Print the results
-                System.out.format("%s\n", city);
-            }
-            //endregion
+        // 4. Process the result set
+        System.out.format("\nTable City --------------------\n");
+        System.out.format("%s\n", "City");
+        while (rs.next()) {
+            String city = rs.getString("City");
+            // Simply Print the results
+            System.out.format("%s\n", city);
+        }
+        //endregion
 
 //region    SELECT * FROM PersonBook //
-            // 3. executing SELECT query
-            String query="Select " +
-                    "(SELECT Surname FROM person WHERE IDPerson=P.IDPerson) AS Surname, " +
-                    "(SELECT BookName FROM book WHERE IDBook=P.IDBook) AS BookName "+
-                    "FROM personbook AS P";
-            rs=statement.executeQuery(query);
+        // 3. executing SELECT query
+        String query = "SELECT " +
+                "(SELECT Surname FROM person WHERE IDPerson=P.IDPerson) AS Surname, " +
+                "(SELECT BookName FROM book WHERE IDBook=P.IDBook) AS BookName " +
+                "FROM personbook AS P";
+        rs = statement.executeQuery(query);
 
-            // 4. Process the result set
-            System.out.format("\nJoining Table PersonBook --------------------\n");
-            System.out.format("%-15s %s\n", "Surname", "BookName");
-            while (rs.next())
-            {
-                String surname = rs.getString("Surname");
-                String bookName = rs.getString("BookName");
-                // Simply Print the results
-                System.out.format("%-15s %s\n", surname, bookName);
-            }
-            //endregion
+        // 4. Process the result set
+        System.out.format("\nJoining Table PersonBook --------------------\n");
+        System.out.format("%-15s %s\n", "Surname", "BookName");
+        while (rs.next()) {
+            String surname = rs.getString("Surname");
+            String bookName = rs.getString("BookName");
+            // Simply Print the results
+            System.out.format("%-15s %s\n", surname, bookName);
+        }
+        //endregion
 
     }
 
@@ -148,12 +156,12 @@ private static final String url ="jdbc:sqlserver://ANDRIY-PC:1433;database=DB_JD
         Scanner input = new Scanner(System.in);
         System.out.println("Input name city what you want to update: ");
         String city = input.next();
-        System.out.println("Input new name city for %s: "+ city);
+        System.out.println("Input new name city for %s: " + city);
         String citynew = input.next();
 
         // 3. executing SELECT query
 // 1
-        statement.execute("UPDATE city SET City='"+citynew+"' WHERE City='"+city+"';");
+        statement.execute("UPDATE city SET City='" + citynew + "' WHERE City='" + city + "';");
 
 // 2  Returns count of updated rows
 //        int n=statement.executeUpdate("UPDATE city SET City='"+citynew+"' WHERE City='"+city+"';");
@@ -177,10 +185,10 @@ private static final String url ="jdbc:sqlserver://ANDRIY-PC:1433;database=DB_JD
         // 3. executing SELECT query
         //   PreparedStatements can use variables and are more efficient
         PreparedStatement preparedStatement;
-        preparedStatement=connection.prepareStatement("INSERT city VALUES (?)");
+        preparedStatement = connection.prepareStatement("INSERT city VALUES (?)");
         preparedStatement.setString(1, newcity);
-        int n=preparedStatement.executeUpdate();
-        System.out.println("Count rows that inserted: "+n);
+        int n = preparedStatement.executeUpdate();
+        System.out.println("Count rows that inserted: " + n);
 
     }
 
@@ -192,10 +200,10 @@ private static final String url ="jdbc:sqlserver://ANDRIY-PC:1433;database=DB_JD
         // 3. executing SELECT query
         //   PreparedStatements can use variables and are more efficient
         PreparedStatement preparedStatement;
-        preparedStatement=connection.prepareStatement("DELETE FROM city WHERE City=?");
+        preparedStatement = connection.prepareStatement("DELETE FROM city WHERE City=?");
         preparedStatement.setString(1, city);
-        int n=preparedStatement.executeUpdate();
-        System.out.println("Count rows that deleted: "+n);
+        int n = preparedStatement.executeUpdate();
+        System.out.println("Count rows that deleted: " + n);
     }
 
     private static void CallProcedureForInsertToPersonBook() throws SQLException {
@@ -206,16 +214,15 @@ private static final String url ="jdbc:sqlserver://ANDRIY-PC:1433;database=DB_JD
         String book = input.next();
 
         CallableStatement callableStatement;
-        callableStatement= connection.prepareCall("{call InsertPersonBook(?, ?)}");
-        callableStatement.setString("SurmanePersonIn",surname);
-        callableStatement.setString("BookNameIN",book);
+        callableStatement = connection.prepareCall("{call InsertPersonBook(?, ?)}");
+        callableStatement.setString("SurmanePersonIn", surname);
+        callableStatement.setString("BookNameIN", book);
         ResultSet rs = callableStatement.executeQuery();
 
-        while (rs.next())
-        {
+        while (rs.next()) {
             String msg = rs.getString(1);
             // Simply Print the results
-            System.out.format("\nResult: "+msg);
+            System.out.format("\nResult: " + msg);
         }
     }
 
